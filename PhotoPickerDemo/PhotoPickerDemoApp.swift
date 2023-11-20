122889266f126df265fa9fb365dacc54350b8faf
David Veeneman <davidv@veeneman.com>
2023-11-19 16:12:31 -0800

    Initial Commit

---
+++ PhotoPickerDemo/PhotoPickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerDemoApp.swift
+//  PhotoPickerDemo
+//
+//  Created by David Veeneman on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
