d1b4e72e26f9c2f72b090e1c321bf62255dfe472
David Veeneman <davidv@veeneman.com>
2023-11-20 07:45:53 -0800

    2023/11/20: Working example of 2023 PhotosPicker and import of selected photo.

--- PhotoPickerDemo/ContentView.swift
+++ PhotoPickerDemo/ContentView.swift
@@ -4,21 +4,48 @@
 //
 //  Created by David Veeneman on 11/19/23.
 //
+//  From "SwiftUI PhotoPicker: A Hands-On Guide with Examples"
+//  https://softwareanders.com/swiftui-photopicker-a-hands-on-guide-with-examples/
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var selectedPhoto: PhotosPickerItem? = nil
+    @State var uiImage: UIImage?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            PhotosPicker(
+                selection: $selectedPhoto,
+                matching: .any(of: [.images, .not(.livePhotos)]),
+                photoLibrary: .shared()) {
+                    Text("Select a photo from PhotoPicker")
+                }
+            
+            if uiImage != nil {
+                Image(uiImage: uiImage!)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+            }
         }
-        .padding()
+        .onChange(of: selectedPhoto) { result in
+            Task {
+                do {
+                    if let data = try await selectedPhoto?.loadTransferable(type: Data.self) {
+                        if let uiImage = UIImage(data: data) {
+                            self.uiImage = uiImage
+                        }
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                    selectedPhoto = nil
+                }
+            }
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
